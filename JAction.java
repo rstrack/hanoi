57145400f8b9b5433bee6e20ea2ed6dfeee390c9
Rafael Strack <strackinado@gmail.com>
2022-08-02 16:32:19 -0300

    UCS: resultado obtido para K=4 em 8min 41seg

--- JAction.java
+++ JAction.java
@@ -10,42 +10,36 @@ public class JAction extends DynamicAction {
     public static final String D1P2P3 = "mover(d1,p2,p3)";
     public static final String D1P3P1 = "mover(d1,p3,p1)";
     public static final String D1P3P2 = "mover(d1,p3,p2)";
-    
     public static final String D2P1P2 = "mover(d2,p1,p2)";
     public static final String D2P1P3 = "mover(d2,p1,p3)";
     public static final String D2P2P1 = "mover(d2,p2,p1)";
     public static final String D2P2P3 = "mover(d2,p2,p3)";
     public static final String D2P3P1 = "mover(d2,p3,p1)";
     public static final String D2P3P2 = "mover(d2,p3,p2)";
-    
     public static final String D3P1P2 = "mover(d3,p1,p2)";
     public static final String D3P1P3 = "mover(d3,p1,p3)";
     public static final String D3P2P1 = "mover(d3,p2,p1)";
     public static final String D3P2P3 = "mover(d3,p2,p3)";
     public static final String D3P3P1 = "mover(d3,p3,p1)";
     public static final String D3P3P2 = "mover(d3,p3,p2)";
-    
     public static final String D4P1P2 = "mover(d4,p1,p2)";
     public static final String D4P1P3 = "mover(d4,p1,p3)";
     public static final String D4P2P1 = "mover(d4,p2,p1)";
     public static final String D4P2P3 = "mover(d4,p2,p3)";
     public static final String D4P3P1 = "mover(d4,p3,p1)";
     public static final String D4P3P2 = "mover(d4,p3,p2)";
-    
     public static final String D5P1P2 = "mover(d5,p1,p2)";
     public static final String D5P1P3 = "mover(d5,p1,p3)";
     public static final String D5P2P1 = "mover(d5,p2,p1)";
     public static final String D5P2P3 = "mover(d5,p2,p3)";
     public static final String D5P3P1 = "mover(d5,p3,p1)";
     public static final String D5P3P2 = "mover(d5,p3,p2)";
-    
     public static final String D6P1P2 = "mover(d6,p1,p2)";
     public static final String D6P1P3 = "mover(d6,p1,p3)";
     public static final String D6P2P1 = "mover(d6,p2,p1)";
     public static final String D6P2P3 = "mover(d6,p2,p3)";
     public static final String D6P3P1 = "mover(d6,p3,p1)";
     public static final String D6P3P2 = "mover(d6,p3,p2)";
-    
     public static final String D7P1P2 = "mover(d7,p1,p2)";
     public static final String D7P1P3 = "mover(d7,p1,p3)";
     public static final String D7P2P1 = "mover(d7,p2,p1)";
